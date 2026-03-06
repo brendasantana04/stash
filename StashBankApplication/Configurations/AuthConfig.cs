1391850610616b4c0b83686985085601ca0f1e79
brenda <127804558+brendasantana04@users.noreply.github.com>
2026-03-06 17:53:03 -0300

    add: configurações de token e hash

---
+++ StashBankApplication/Configurations/AuthConfig.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+namespace StashBankApplication.Configurations
+{
+    public static class AuthConfig
+    {
+        public static IServiceCollection AddAuthConfiguration (
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            var tokenConfigurations = new Auth.Config.TokenConfiguration();
+           
+            configuration.GetSection("TokenConfigurations")
+                .Bind(tokenConfigurations);
+
+            services.AddSingleton(tokenConfigurations);
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme 
+                    = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+
+            return services;
+        }
+    }
+}
